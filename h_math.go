bcc9914a422ac637c88b437cfdc48086c671e367
huyang <982189800@qq.com>
2018-05-04 00:23:49 +0800

    find number of contain one

--- h_math.go
+++ h_math.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//"time"
 )
 
 func main() {
@@ -10,10 +10,19 @@ func main() {
 	//fmt.Println(fmt.Sprintf("25的阶乘末尾有%d个零", find(25)))
 	//fmt.Println(fmt.Sprintf("50的阶乘末尾有%d个零", find(50)))
 	//utils.Trace("查找素数")()
-	start := time.Now()
-	findPrimeNumber(2348153532453467788)
-	duration := time.Since(start)
-	fmt.Println("耗时：", duration)
+	//start := time.Now()
+	//ii := 23
+	fmt.Println("10 % 10", 10%10)
+	for ii := 8; ii < 23; ii++ {
+		//fmt.Println("shuzi", ii)
+		fmt.Println(ii, "以内有", findNumberOfContainOne(ii), "个含有1的整数, :", count1(ii))
+	}
+
+	fmt.Println(10, "以内有", findNumberOfContainOne(10), "个含有1的整数")
+	//findPrimeNumber(1000)
+	//duration := time.Since(start)
+	//fmt.Println("耗时：", duration)
+
 }
 
 // 设计一个算法，计算出n阶乘中尾部零的个数
@@ -23,6 +32,58 @@ func find(n int) (res int) {
 		n = n / 5
 	}
 	return
+}
+
+// 1~n 里有多少个包含1的整数
+func findNumberOfContainOne(n int) (res int) {
+	//fmt.Println("n", n)
+
+	offset := 1
+	for n > 0 {
+		a := n / 10
+		b := n % 10
+		if b > 0 {
+			if a == 0 {
+				res += 1
+			} else if a == 1 {
+				res += a + b + 1
+			} else {
+				res += a + b + offset
+			}
+		} else {
+			if a > 1 {
+				res += a + 1
+			} else {
+				res += offset + a + 1
+			}
+		}
+		offset *= 10
+		n = n / 10
+	}
+	//res--
+	return res
+}
+
+func count1(n int) int {
+	count := 0
+	j := 0
+	val := 0
+	for i := 1; i <= n; i++ {
+		val = i
+		for val > 0 {
+			j = val % 10
+			if j == 1 {
+				count++
+				break
+			}
+			val = val / 10
+		}
+	}
+	return count
+}
+
+func count_one_bits(n int) (int)  {
+
 }
 
 // 给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符。
@@ -53,8 +114,9 @@ func Add(a, b int) int {
 func findPrimeNumber(n int) {
 	for i := n; i >= 2; i-- {
 		if isPrinme(i) {
-			fmt.Println(n, "以内最大的素数是：", i)
-			return
+			fmt.Println(n, "以内的素数：", i)
+			//fmt.Println(n, "以内最大的素数是：", i)
+			//return
 		} else {
 			//fmt.Println("---", i)
 		}
@@ -62,11 +124,14 @@ func findPrimeNumber(n int) {
 }
 
 func isPrinme(m int) bool {
-	if m % 2 == 0 {
+	if m == 2 {
+		return true
+	}
+	if m%2 == 0 {
 		return false
 	}
-	for index := 3; index * index < m; index += 2 {
-		if m % index == 0 {
+	for index := 3; index*index <= m; index += 2 {
+		if m%index == 0 {
 			return false
 		}
 	}
